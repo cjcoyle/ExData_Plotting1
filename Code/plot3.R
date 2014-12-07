e3d75e8e15ba4df2be6dc87635d7624e5b2584d8
Christopher Coyle <cjcoyle2k@yahoo.com>
2014-12-07 12:12:42 -0500

    Create Code Directory and Move Code Files Into It

---
+++ Code/plot3.R
@@ -0,0 +1,14 @@
+# Download and Clean Data
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+data$DateTime <- dmy_hms(paste(data$Date, data$Time))
+data$Date <- dmy(data$Date)
+data$Time <- hms(data$Time)
+dataChart <- subset(data, year(Date) == 2007 & month(Date) == 2 & (day(Date) == 1 | day(Date) == 2))
+
+# Create Chart
+png("Rplot3.png", width = 480, height = 480)
+with(dataChart, plot(DateTime, Sub_metering_1, type = "l", col = "black", ylab = "Energy sub metering", xlab = ""))
+with(dataChart, lines(DateTime, Sub_metering_2, col = "red"))
+with(dataChart, lines(DateTime, Sub_metering_3, col = "blue"))
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1)
+dev.off()
